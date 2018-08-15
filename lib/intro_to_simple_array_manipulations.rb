672f2a551e516ca408eedf145af337edf2978840
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-15 22:45:31 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -33,3 +33,38 @@ while counter<2 do
 end
 new_array
 end
+
+def using_concat(array1,array2)
+array1.concat(array2)
+end
+
+def using_insert(array,new_element)
+  array.insert(4,new_element)
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
